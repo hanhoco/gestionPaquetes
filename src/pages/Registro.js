8c5141d7866a6c564e76254e758a994f7663b5f1
AngelaBarriosP <112993987+AngelaBarriosP@users.noreply.github.com>
2022-12-04 10:50:17 -0500

    Cambio de forma

--- src/pages/Registro.js
+++ src/pages/Registro.js
@@ -5,37 +5,37 @@ import Form from 'react-bootstrap/Form';
 function Registro (props){
     return(
         <Form>
-            <h1 className='pb-5'>{props.title}</h1>
+          <h1 className='pb-5'>{props.title}</h1>
             
-      <Form.Group className="mb-3" controlId="nombre">
-        <Form.Label>Nombre</Form.Label>
-        <Form.Control type="input" placeholder="Ingresar nombre" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="nombre">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="input" placeholder="Ingresar nombre" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="documento">
-        <Form.Label>Documento</Form.Label>
-        <Form.Control type="input" placeholder="Ingresar documento" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="documento">
+            <Form.Label>Documento</Form.Label>
+            <Form.Control type="input" placeholder="Ingresar documento" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="usuario">
-        <Form.Label>Usuario</Form.Label>
-        <Form.Control type="input" placeholder="Ingresar usuario" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="usuario">
+            <Form.Label>Usuario</Form.Label>
+            <Form.Control type="input" placeholder="Ingresar usuario" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="input" placeholder="Ingresa email" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="input" placeholder="Ingresa email" />
+          </Form.Group>
 
 
-      <Button variant="primary" type="submit">
-        Crear
-      </Button>
+      <Button href="/order-login" type="cancelar">Cancelar</Button>
+      <Button variant="primary" type="submit">Crear</Button>
+
     </Form>
 
     )
